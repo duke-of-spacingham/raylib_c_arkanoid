242ac31e6d1697f3d53968bf9162528211bb972a
duke-of-spacingham <duke.of.spacingham@gmail.com>
2025-12-12 00:46:21 +0200

    Updated preparatory installations script

--- raylib_dependencies_install.sh
+++ raylib_dependencies_install.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-# Dependencies for compiling raylib
+# Dependencies for compiling raylib and its games
 sudo apt-get install git make g++ cmake x11-apps libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev libxkbcommon-dev
