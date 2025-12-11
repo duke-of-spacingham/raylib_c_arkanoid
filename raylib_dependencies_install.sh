0b7627cdf5eb7ea27bd9d34fa357a2a1a5dd4fb3
duke-of-spacingham <duke.of.spacingham@gmail.com>
2025-12-12 00:42:14 +0200

    Added dependencies installer script

---
+++ raylib_dependencies_install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# Dependencies for compiling raylib
+sudo apt-get install git make g++ cmake
+sudo apt-get install libasound2-dev libx11-dev libxrandr-dev libxi-dev
+sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev
+sudo apt-get install libxinerama-dev libwayland-dev libxkbcommon-dev
