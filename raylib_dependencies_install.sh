9fc7ef737465cdb5c406b3b98918e09c9759c21e
duke-of-spacingham <duke.of.spacingham@gmail.com>
2025-12-12 00:45:00 +0200

    Updated the preparatory installing script to have a single installation line

--- raylib_dependencies_install.sh
+++ raylib_dependencies_install.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 # Dependencies for compiling raylib
-sudo apt-get install git make g++ cmake
-sudo apt-get install libasound2-dev libx11-dev libxrandr-dev libxi-dev
-sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev
-sudo apt-get install libxinerama-dev libwayland-dev libxkbcommon-dev
+sudo apt-get install git make g++ cmake x11-apps libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev libxkbcommon-dev
